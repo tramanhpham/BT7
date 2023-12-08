d900a2d2e749244895ac8704d3e69b89cd907dc8
Phạm Trâm Anh <93437075+tramanhpham@users.noreply.github.com>
2023-12-08 10:49:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,12 @@ uni_rate = st.number_input('Insert University Rating')
 sop = st.number_input('Insert SOP')
 lor = st.number_input('Insert LOR')
 cgpa = st.number_input('Insert CGPA')
+research = st.radio('Choose Research', [0, 1], index=None)
+
+if gre is not None and toefl is not None and uni_rate is not None and sop is not None and lor is not None and cgpa is not None and research is not None:
+    if st.button('Predict'):
+        feature_vector = np.array([gre, toefl, uni_rate, sop, lor, cgpa, research]).reshape(1,-1)
+        result = str((model.predict(feature_vector)[0])[0])
+
+        st.header('Result')
+        st.text(result)
